fd9538f50c4f87dfc81dffe9fe3b94665a2af21f
anakraharja <anak.raharja20@gmail.com>
2024-05-30 06:41:01 +0700

    API: add middleware in route logout

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,10 @@ Route::middleware('auth:api')->group(function(){
 });
 Route::prefix('mobile')->name('mobile.')->group(function () {
     Route::post('login', 'Api\Mobile\AuthController@login');
-    Route::get('logout', 'Api\Mobile\AuthController@logout');
     Route::resource('class', 'Api\Mobile\ClassController');
 
     Route::group(['middleware' => 'check-token-api'], function () {
+        Route::get('logout', 'Api\Mobile\AuthController@logout');
         Route::get('student', 'Api\Mobile\StudentController@index');
         Route::post('student', 'Api\Mobile\StudentController@store');
     });
